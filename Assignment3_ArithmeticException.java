dcc480cdf3ca92b963b69dff1839cbf8ec10e939
banils <99386668+banils@users.noreply.github.com>
2022-02-11 16:24:41 +0530

    Handle a Arithmetic Exception

---
+++ Assignment3_ArithmeticException.java
@@ -0,0 +1,24 @@
+package Assignment3;
+
+class Assignment3_ArithmeticException{ 
+  
+	   void method1() throws ArithmeticException{  
+		throw new ArithmeticException("Calculation error"); 
+	   }  
+	   void method2() throws ArithmeticException{  
+		method1();  
+	   }  
+	   void method3(){  
+		try{  
+		   method2();  
+		}
+		catch(ArithmeticException e){
+		   System.out.println("ArithmeticException handled");
+		}  
+	   }  
+	   public static void main(String args[]){  
+		Arithmeticexception obj=new Arithmeticexception();  
+		obj.method3();  
+		System.out.println("End Of Program");  
+	   }  
+	}
